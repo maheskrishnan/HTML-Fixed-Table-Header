dec7c833b7ccc61c68acac1f6b49f6f080a43004
Maheswaran Krishnan <reachmahes2k@gmail.com>
2012-01-19 14:51:40 -0500

    initial version -

---
+++ as a plugin/jquery.scrolify.js
@@ -0,0 +1,60 @@
+
+/* Author: Maheswaran Krishnan */
+/* simple html table with fixed header jQuery plug-in */
+/* https://github.com/maheskrishnan/HTML-Fixed-Table-Header */
+
+(function($){
+	$.fn.extend({ 
+		//pass the options variable to the function
+		scrolify: function(options) {
+		
+    function scrolify(tblAsJQueryObject, height){
+        var oTbl = tblAsJQueryObject;
+
+        // for very large tables you can remove the four lines below
+        // and wrap the table with <div> in the mark-up and assign
+        // height and overflow property  
+        var oTblDiv = $("<div/>");
+        oTblDiv.css('height', height);
+        oTblDiv.css('overflow','scroll');               
+        oTbl.wrap(oTblDiv);
+
+        // save original width
+        oTbl.attr("data-item-original-width", oTbl.width());
+        oTbl.find('thead tr td').each(function(){
+            $(this).attr("data-item-original-width",$(this).width());
+        }); 
+        oTbl.find('tbody tr:eq(0) td').each(function(){
+            $(this).attr("data-item-original-width",$(this).width());
+        });                 
+
+
+        // clone the original table
+        var newTbl = oTbl.clone();
+
+        // remove table header from original table
+        oTbl.find('thead tr').remove();                 
+        // remove table body from new table
+        newTbl.find('tbody tr').remove();   
+
+        oTbl.parent().parent().prepend(newTbl);
+        newTbl.wrap("<div/>");
+
+        // replace ORIGINAL COLUMN width                
+        newTbl.width(newTbl.attr('data-item-original-width'));
+        newTbl.find('thead tr td').each(function(){
+            $(this).width($(this).attr("data-item-original-width"));
+        });     
+        oTbl.width(oTbl.attr('data-item-original-width'));      
+        oTbl.find('tbody tr:eq(0) td').each(function(){
+            $(this).width($(this).attr("data-item-original-width"));
+        });                 
+    }			
+		
+			return this.each(function() {
+				var o = options;
+				scrolify($(this),o.height);								
+			});
+		}
+	});
+})(jQuery);	
---
+++ readme.md
@@ -0,0 +1,10 @@
+this is the solution outline.
+
+clone the table which needs to have fixed header and place the cloned copy on top of original
+remove the table body from top table
+remove the table header from bottom table
+adjust the column widths. (we are remembering the original column widths)
+
+below is the code. here's the demo - http://gomathikrishna.net/demos/scroll.html
+
+this solution works in chrome & ie. since this is based on jquery this should work in other jquery supported browsers as well.
